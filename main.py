177c2996d792f34f02953d4646eb1a361b8f3f7e
JD Wheelehan <jdwheelehan@gmail.com>
2017-09-27 18:38:02 -0500

    finished project minus debugging

--- main.py
+++ main.py
@@ -26,37 +26,42 @@ def validate_info():
     vpass_error = ''
     email_error = ''
 
-    if not is_integer(hours):
-        hours_error = 'Not a valid integer'
-        hours = ''
-    else:
-        hours = int(hours)
-        if hours > 23 or hours <0:
-            hours_error = 'Hour value out of range (0-23)'
-            hours = ''
-
-    if not is_integer(minutes):
-        minutes_error = 'Not a valid integer'
-        minutes = ''
-    else:
-        minutes = int(minutes)
-        if minutes > 59 or minutes < 0:
-            minutes_error = 'Minutes value out of range (0-59)'
-            minutes = ''
-
-    if not user_error and not pass_error and not vpass_error and not email_error:
+    if username == '':
+        user_error = 'Please enter a username.'
         
+    if len(username) < 3 or len(username) > 20 or ' ' in username:
+            user_error = 'Username invalid'
+    
+    if password == '':
+        pass_error = 'Please enter a password.'
+
+    if len(password) < 3 or len(password) > 20 or ' ' in password:
+            pass_error = 'Password invalid.'
+
+    if vpassword == '':
+        vpass_error = 'Please verify the password.'
+
+    if vpassword != password:
+        vpass_error = "Passwords do not match."
+
+    if len(email) >= 1 and len(email) < 3 or len(email) > 20 or ' ' in email or '@' not in email:
+        email_error = "Enter valid email please."
+    
+    if not user_error and not pass_error and not vpass_error and not email_error:
+      
         return redirect('/welcome')
     else:
         template = jinja_env.get_template('form.html')
         return template.render(user_error=user_error,
-            pass_error=pass_error, vpass_error=vpass_error, email_error=email_error, user=username,
+            pass_error=pass_error, vpass_error=vpass_error, 
+            email_error=email_error, user=username,
             email=email)
 
 
 
 @app.route('/welcome')
-def valid_time():
+def valid_signin():
     name = request.args.get('user')
+    
     return '<h1>Welcome {0}!!!</h1>'.format(name)
 app.run()
